57b8401cffa6854260edc00f3c1ca26c031bd0de
rayko <rayko@rayko-server.(none)>
2009-10-05 16:45:40 -0300

    Index updated

--- www/config/routes.rb
+++ www/config/routes.rb
@@ -54,4 +54,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => 'main', :action => 'index'
 end
