694a7a4f122ac9cbba6d267cd16a2be37043e0dc
rayko <rayko@raykosite.com.ar>
2010-05-07 12:40:16 -0300

    Added a menu and improved work show

--- www/app/controllers/comments_controller.rb
+++ www/app/controllers/comments_controller.rb
@@ -40,13 +40,20 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.xml
   def create
-    @comment = Comment.new(params[:comment])
-
+    debugger
+    unless params[:work_id].nil?
+      @comment = Comment.new
+      @comment.name = params[:name]
+      @comment.mail = params[:mail]
+      @comment.comment = params[:comment]
+      @comment.work_id = params[:work_id]
+    else
+      @comment = Comment.new(params[:comment])
+    end
     respond_to do |format|
       if @comment.save
         flash[:notice] = 'Comment was successfully created.'
-        format.html { redirect_to(@comment) }
-        format.xml  { render :xml => @comment, :status => :created, :location => @comment }
+        format.html { redirect_to(work_path(params[:work_id])) }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
